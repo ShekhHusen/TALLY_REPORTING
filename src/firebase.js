bfec27e05b472b9e66b449fa1b98e507eff1bb84
ShekhHusen <43610459+ShekhHusen@users.noreply.github.com>
2026-09-05 12:51:40 +0545

    FIREBASE CONFIG

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAkGL0wqhK3h6ausPyCdAfvBud7NmqgRg",
-  authDomain: "reporting-c532c.firebaseapp.com",
-  projectId: "reporting-c532c",
-  storageBucket: "reporting-c532c.firebasestorage.app",
-  messagingSenderId: "563591207439",
-  appId: "1:563591207439:web:7100ace8701fc188eda17a",
-  measurementId: "G-EHQHCVHXNT"
+  apiKey: "AIzaSyCrqNgwRJ0RqZqZRJlETX7nTgwEltCO33g",
+  authDomain: "tally-reporting.firebaseapp.com",
+  projectId: "tally-reporting",
+  storageBucket: "tally-reporting.firebasestorage.app",
+  messagingSenderId: "896448159679",
+  appId: "1:896448159679:web:cbf9ccaa50fb73708a0b0a",
+  measurementId: "G-PXGY6S82Q2"
 };
 
 const app = initializeApp(firebaseConfig);
