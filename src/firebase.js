5f0d4c2221a99a082ffeedfbd3dd559742862ad0
ShekhHusen <43610459+ShekhHusen@users.noreply.github.com>
2026-09-05 12:38:06 +0545

    Update Firebase configuration details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7_yuapmis0lvNyEgn7lx7zURh82MdDN8",
-  authDomain: "reporting-cfd7f.firebaseapp.com",
-  projectId: "reporting-cfd7f",
-  storageBucket: "reporting-cfd7f.firebasestorage.app",
-  messagingSenderId: "960616847306",
-  appId: "1:960616847306:web:c5092bbf28e78c9619e133",
-  measurementId: "G-QST3GBXPZC"
+  apiKey: "AIzaSyCAkGL0wqhK3h6ausPyCdAfvBud7NmqgRg",
+  authDomain: "reporting-c532c.firebaseapp.com",
+  projectId: "reporting-c532c",
+  storageBucket: "reporting-c532c.firebasestorage.app",
+  messagingSenderId: "563591207439",
+  appId: "1:563591207439:web:7100ace8701fc188eda17a",
+  measurementId: "G-EHQHCVHXNT"
 };
 
 const app = initializeApp(firebaseConfig);
