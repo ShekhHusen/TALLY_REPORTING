bfa44a010932d890340453653f3168d2152185fe
ShekhHusen <43610459+ShekhHusen@users.noreply.github.com>
2026-09-12 09:48:30 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDxUdNuacE9J08BoebrY4Ax63CD4phZXj4",
+  authDomain: "jbmt-reporting.firebaseapp.com",
+  projectId: "jbmt-reporting",
+  storageBucket: "jbmt-reporting.firebasestorage.app",
+  messagingSenderId: "554546722753",
+  appId: "1:554546722753:web:c6be832b4d00fbe6bfe140",
+  measurementId: "G-JGCWKJPQTG"
 };
 
 const app = initializeApp(firebaseConfig);
